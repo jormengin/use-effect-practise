e5d99da75150e6015f7011ec802d23946a44d927
Jorge Mendez <jorge.mendezgines@gmail.com>
2023-03-04 13:01:52 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,13 @@ function App() {
 
   const [showCounter, setShowCounter] = useState(true)
 
+  const handleShow =()=>{
+    setShowCounter(prev=>!prev)
+  }
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <button onClick={handleShow}>{showCounter ? 'Hide counter' : 'Show counter'}</button>
       {showCounter && <Counter />}
     </div>
   );
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,18 +4,44 @@ export default function Counter() {
 
   const [count, setCount] = useState(0);
   const [dynamicClass, setDynamicClass] = useState('text');
+  const [color, setColor] = useState('')
 
-  // First iteration
+  useEffect(()=>{
+    let randomNum = Math.floor(Math.random()*10)
+    setCount(randomNum)
+    return ()=>{
+      setCount(0)
+      setDynamicClass('text')
+      console.log('All clean here. Count:', count, 'Class:', dynamicClass);
+    }
+  },[])
 
+  useEffect(()=>{
+    if(count<=0){
+      setColor('red')
+    } else if(count<= 5){
+      setColor('orange')
+    } else {
+      setColor('green')
+    }
+    
+  },[count, dynamicClass])
+
+const handleIncrease =()=>{
+  setCount(prev=>prev+1)
+}
+const handleDecrease =()=>{
+  setCount(prev=>prev-1)
+}
   // Second and Fourth iteration
 
   // Third iteration
 
   return (
     <div>
-      <p>Counter: <span className={dynamicClass}>{count}</span></p>
-      <button>+ Increase</button>
-      <button>- Decrease</button>
+      <p className={color}>Counter: <span className={dynamicClass}>{count}</span></p>
+      <button onClick={handleIncrease}>+ Increase</button>
+      <button onClick={handleDecrease }>- Decrease</button>
     </div>
   )
 }
